ae2b4caf67454fb107bf001454b9282cfda96891
GlebNesterov <gnesterov01@educantabria.es>
2026-01-20 17:21:01 +0100

    crear un archivo
    crear un archivo main en la carpeta src

---
+++ src/proyecto-equipo_1/src/main/java/com/mycompany/proyecto/equipo_1/ProyectoEquipo_1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto.equipo_1;
+
+/**
+ *
+ * @author DAW104
+ */
+public class ProyectoEquipo_1 {
+
+    public static void main(String[] args) {
+        int[][] pro = new int[5][5];
+        
+        for (int i = 0; i < pro.length; i++) {
+            for (int j = 0; j < pro.length; j++) {
+                System.out.print(pro[i][j]);
+            }
+            System.out.println("");
+        }
+        
+    }
+}
