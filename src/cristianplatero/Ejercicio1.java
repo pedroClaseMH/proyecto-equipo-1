98440f996d9419bc734ffd221fd268b5bb64244c
CristianPlatero <cplateroz2501@educantabria.es>
2026-02-24 17:26:56 +0100

    CristianPlatero-#16-dev_app_y_creacion_README
    Se ha añadido la aplicación requerida y el README.md de nuestra aplicación.

---
+++ src/cristianplatero/Ejercicio1.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hoja1;
+
+/**
+ *
+ * @author daw1
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        
+    boolean casado = true;
+    final int MAXIMO = 999999; // si queremos un valor constante, ponemos final delante y la variable en MAY.
+    byte diasem = 1;
+    short diaanual = 300;
+    char sexo = 'M'; // otro valor valido seria V
+    long miliseg = 1298332800000L;
+    double totalfactura = 10350.678;
+    long poblacion = 6775235741L;
+    // Programación - primer ejerciio de JAVA - Cristian Platero
+    
+    
+    
+    // Ejercicio 2
+    
+        System.out.println("casado " + casado);
+        System.out.println("MAXIMO " + MAXIMO);
+        System.out.println("dia de la semana " + diasem);   
+        System.out.println("dia del año " + diaanual);
+        System.out.println("sexo " + sexo);
+        System.out.println("milisegunods " + miliseg);
+        System.out.println("total factura " + totalfactura);
+        System.out.println("poblacion " + poblacion);
+        
+        
+    // otro ejercicio de clase
+        System.out.println("EJEMPLO DE SALIDA DE DATOS A TRAVES DE LA CONSOLA");
+        
+        System.out.print("EJEMPLO DE UTILIZACION DE PRINT");
+        
+    }
+}
